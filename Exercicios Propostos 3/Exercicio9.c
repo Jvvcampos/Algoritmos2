689bde848aadeee25ca5647fb5f3161d61ba0dad
BadMickey <joaovitorcampos161@gmail.com>
2022-04-20 17:50:54 -0300

    Enunciado do Exercicio 9 corrigido

--- Exercicios Propostos 3/Exercicio9.c
+++ Exercicios Propostos 3/Exercicio9.c
@@ -1,11 +1,8 @@
 /*
-Exercício 8. (Busca sequencial versus Busca binária) Considere os dois últimos
-programas. Suponha que você queira pesquisar a ocorrência do número x. O
-objetivo deste exercício é contar o número de testes/comparações de x com os
-elementos do vetor, que cada um dos programas precisa fazer para tomar uma
-decisão. Para isso, em cada um dos programas, acrescente um contador para
-determinar este número. Compare o desempenho de ambos os programas,
-considerando o número de comparações realizadas.
+Exercício 9. Escreva um programa que leia do teclado uma frase e a coloque em
+um vetor de caracteres. Entre as palavras que compõem a frase, pode haver um
+ou mais espaços em branco ou sinais de pontuação ( , e . ) . O programa deve
+escrever na tela as palavras que compõem a frase, uma por linha.
 */
 
 #include <stdio.h>
