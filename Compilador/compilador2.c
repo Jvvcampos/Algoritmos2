1c282daeaf2c931641b63e578508c6c7d6829cff
BadMickey <joaovitorcampos161@gmail.com>
2022-07-13 03:11:56 -0300

    Inicio da implementação das restrições - Restrições de Start e Stop(Apenas reconhece comandos se usado o Start e etc) - Restrições da main

--- Compilador/compilador2.c
+++ Compilador/compilador2.c
@@ -61,15 +61,17 @@ void tirar_espacos(char linha[])//Essa função removerá espaços duplos no código,
     }
 }
 
-void verifica_var(char linha[])
+void verifica_var(char linha[])//função para verificar o comando var
 {
   if(linha[4]=='i' && linha[5]=='n' && linha[6]=='t' && linha[7]==' ')//Váriavel como tipo inteiro
     {
+        printf("Váriavel de tipo int\n");
         for(int i = 8; i<strlen(linha)-2; i++)
         {
             if(linha[i]=='>' && linha[i+1]=='=' || linha[i]=='<' && linha[i+1]=='=' || linha[i]=='=' && linha[i+1]=='=' || linha[i]=='!' && linha[i+1]=='=')
             {
-                printf("Uso de operadores lógicos indevido, somente é permitido usar = para atribuir valores");
+                printf("Uso de operadores lógicos indevido, somente é permitido usar = para atribuir valores!\n");
+                printf("Conserte o erro e tente novamente!\n");
                 printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
                 exit(0);
             }
@@ -78,17 +80,19 @@ void verifica_var(char linha[])
         {
             printf("O comando var está faltando o ';' no final da linha %d!!\n", linhas_lidas);
             printf("Conserte o erro e tente novamente!\n");
-            printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+            printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
             exit(0);
         }
     }
     else if(linha[4]=='f' && linha[5]=='l' && linha[6]=='o' && linha[7]=='a' && linha[8]=='t' && linha[9]==' ')//Váriavel como tipo float
     {
+        printf("Váriavel de tipo float\n");
         for(int i = 10; i<strlen(linha)-2; i++)
         {
             if(linha[i]=='>' && linha[i+1]=='=' || linha[i]=='<' && linha[i+1]=='=' || linha[i]=='=' && linha[i+1]=='=' || linha[i]=='!' && linha[i+1]=='=')
             {
-                printf("Uso de operadores lógicos indevido, somente é permitido usar = para atribuir valores");
+                printf("Uso de operadores lógicos indevido, somente é permitido usar = para atribuir valores!\n");
+                printf("Conserte o erro e tente novamente!\n");
                 printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
                 exit(0);
             }
@@ -97,13 +101,53 @@ void verifica_var(char linha[])
         {
             printf("O comando var está faltando o ';' no final da linha %d!!\n", linhas_lidas);
             printf("Conserte o erro e tente novamente!\n");
-            printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+            printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
             exit(0);
         }
     }
     else if(linha[4]=='c' && linha[5]=='h' && linha[6]=='a' && linha[7]=='r' && linha[8]==' ')
     {
         printf("Váriavel de tipo string\n");
+        int chave_char=0;
+        for(int i = 9; i<strlen(linha)-2; i++)
+        {
+            if(linha[i]=='[')
+            {
+                chave_char+=1;
+            }
+            if(linha[i]==']')
+            {
+                chave_char-=1;
+            }
+            if(linha[i]=='>' && linha[i+1]=='=' || linha[i]=='<' && linha[i+1]=='=' || linha[i]=='=' && linha[i+1]=='=' || linha[i]=='!' && linha[i+1]=='=')
+            {
+                printf("Uso de operadores lógicos indevido, somente é permitido usar = para atribuir valores!\n");
+                printf("Conserte o erro e tente novamente!\n");
+                printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
+                exit(0);
+            }
+            if(chave_char>=1 || chave_char<0)
+            {
+                printf("Erro nos colchetes do tamanho do char na linha %d, foi colocado um colchete de abertura a mais ou um de fechamento!!\n", linhas_lidas);
+                printf("Conserte o erro e tente novamente!\n");
+                printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
+                exit(0);
+            }
+        }
+        if(chave_char!=0)
+        {
+            printf("Erro nos colchetes do tamanho do char na linha %d, não foi fechado o colchete!!\n", linhas_lidas);
+            printf("Conserte o erro e tente novamente!\n");
+            printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
+            exit(0);
+        }  
+        if(linha[strlen(linha)-2]!=';' )
+        {
+            printf("O comando var está faltando o ';' no final da linha %d!!\n", linhas_lidas);
+            printf("Conserte o erro e tente novamente!\n");
+            printf("Foram lidas %d linhas do arquivo!", linhas_lidas);
+            exit(0);
+        }
     }
 }
 
@@ -116,6 +160,11 @@ int main()
     char result[100];
     char *leitura;
     int i;
+    int verificador_start = 0;
+    int verificador_stop = 0;
+    int verificador_main = 0;
+    int verificador_chaves = 0;
+    int verificador_bloco_condicao = 0;
 
     //Digitar o nome do arquivo para leitura
     printf("Digite o nome do arquivo a ser lido pelo compilador: ");
@@ -129,20 +178,109 @@ int main()
         return 0;
     }
 
-    i = 1;
     while(!feof(fp))
     {
         leitura = fgets(linha, 100, fp);
         linhas_lidas++;
         transformar_minusculo(linha);
         preparar_linha(linha);
-        
-        //printf("Linha sem espaï¿½os %d: %s", i, linha);
 
-        if((strncmp(linha, "var", 3)==0))
+        if((strncmp(linha, "!start", 5)==0))
         {
-            verifica_var(linha);
+            verificador_start+=1;
+            if(verificador_start>1)
+            {
+                printf("Um comando de Start a mais foi inserido no código, remova o excedente, lembre-se que o código só irá contar apartir de quando o !start for iniciado!\n", linhas_lidas);
+                printf("Conserte o erro e tente novamente!\n");
+                printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+                exit(0);
+            }
         }
+        if(verificador_start==1)
+        {
+            if((strncmp(linha, "var", 3)==0))
+            {
+                verifica_var(linha);
+            }
+        }
+        if(strncmp(linha, "func void main()", 16)==0 || strncmp(linha, "func void main ()", 17)==0)
+        {
+            if(verificador_main>1)
+            {
+                printf("Houve uma tentativa de abertura de outra main na linha %d, apague-a e deixe seu código dentro de apenas uma função main!\n", linhas_lidas);
+                printf("Conserte o erro e tente novamente!\n");
+                printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+                exit(0);
+            }
+            if(linha[strlen(linha)-2]!='{')
+            {
+                printf("A abertura da main faltou a chave de abertura no final da linha %d!\n", linhas_lidas);
+                printf("Conserte o erro e tente novamente!\n");
+                printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+                exit(0);
+            }
+            else
+            {
+                verificador_main+=1;
+                verificador_chaves+=1;
+            }
+        }
+        /*if(verificador_chaves==1 && verificador_main==1 && verificador_start==1)
+        {
+            if(strncmp(linha, ))
+        }*/
+
+        if(strncmp(linha, "}", 1)==0)
+        {
+            verificador_chaves-=1;
+        }
+        if(strncmp(linha, "!stop", 5) == 0)
+        {   
+            verificador_stop+=1;
+            if(verificador_stop>1)
+            {
+                printf("Um comando de Stop a mais foi inserido no código, remova o excedente, lembre-se que o código irá deixar de verificar depois do !stop\n", linhas_lidas);
+                printf("Conserte o erro e tente novamente!\n");
+                printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+                exit(0);
+            }
+        }
+        i++;
+    }
+    fclose(fp);
+
+    if(verificador_chaves>0)
+    {
+        if(verificador_main==1 && verificador_chaves==1)
+        {
+            printf("O arquivo terminou de ser lido, porém, a main não foi fechada e assim ficou incorreto!\n");
+        }
+        else
+        {
+            printf("O arquivo terminou de ser lido, porém, o código ficou com uma chave a mais e assim, incorreto!\n");
+        }
+    }
+    else if(verificador_chaves<0)
+    {
+        printf("O arquivo terminou de ser lido, porém, o código ficou com uma chave a menos e assim ficou incorreto!\n");
+    }
+
+    if(verificador_stop<1)
+    {
+        printf("O arquivo terminou de ser lido, porém, o código ficou sem o !stop para parar a leitura do código e assim ficou incorreto!\n"); 
+    }
+    if(verificador_stop==1 && verificador_chaves==0)
+    {
+        printf("O código estava totalmente correto!\n");
+    }
+    printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
+    free(fp);
+    free(leitura);
+    return 0;
+}
+
+
+
 
         //if((strncmp(leitura, "if(", 3)==0) || (strncmp(leitura, "if (", 4)==0))
 
@@ -161,14 +299,3 @@ int main()
         {
             printf("linha vï¿½zia\n");
         }*/
-        if(strncmp(linha, "!stop", 5) == 0)
-        {   
-          free(fp);
-          free(leitura);  
-        }
-        i++;
-    }
-    fclose(fp);
-
-    return 0;
-}
