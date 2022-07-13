12c7b51cca755b469f69249e4b4a70f5f299518d
BadMickey <joaovitorcampos161@gmail.com>
2022-07-13 03:20:23 -0300

    Algumas documentações adicionadas e pequenos ajustes

--- Compilador/compilador2.c
+++ Compilador/compilador2.c
@@ -63,7 +63,7 @@ void tirar_espacos(char linha[])//Essa função removerá espaços duplos no código,
 
 void verifica_var(char linha[])//função para verificar o comando var
 {
-  if(linha[4]=='i' && linha[5]=='n' && linha[6]=='t' && linha[7]==' ')//Váriavel como tipo inteiro
+  if(linha[4]=='i' && linha[5]=='n' && linha[6]=='t' && linha[7]==' ')//Variável como tipo inteiro
     {
         printf("Váriavel de tipo int\n");
         for(int i = 8; i<strlen(linha)-2; i++)
@@ -84,7 +84,7 @@ void verifica_var(char linha[])//função para verificar o comando var
             exit(0);
         }
     }
-    else if(linha[4]=='f' && linha[5]=='l' && linha[6]=='o' && linha[7]=='a' && linha[8]=='t' && linha[9]==' ')//Váriavel como tipo float
+    else if(linha[4]=='f' && linha[5]=='l' && linha[6]=='o' && linha[7]=='a' && linha[8]=='t' && linha[9]==' ')//Variável como tipo float
     {
         printf("Váriavel de tipo float\n");
         for(int i = 10; i<strlen(linha)-2; i++)
@@ -105,11 +105,11 @@ void verifica_var(char linha[])//função para verificar o comando var
             exit(0);
         }
     }
-    else if(linha[4]=='c' && linha[5]=='h' && linha[6]=='a' && linha[7]=='r' && linha[8]==' ')
+    else if(linha[4]=='c' && linha[5]=='h' && linha[6]=='a' && linha[7]=='r' && linha[8]==' ')//Variável de tipo char
     {
         printf("Váriavel de tipo string\n");
         int chave_char=0;
-        for(int i = 9; i<strlen(linha)-2; i++)
+        for(int i = 9; i<strlen(linha)-2; i++)//Esse for vai verificar a presença de [] e a garantia dos operadores lógicos
         {
             if(linha[i]=='[')
             {
@@ -164,7 +164,6 @@ int main()
     int verificador_stop = 0;
     int verificador_main = 0;
     int verificador_chaves = 0;
-    int verificador_bloco_condicao = 0;
 
     //Digitar o nome do arquivo para leitura
     printf("Digite o nome do arquivo a ser lido pelo compilador: ");
@@ -185,10 +184,10 @@ int main()
         transformar_minusculo(linha);
         preparar_linha(linha);
 
-        if((strncmp(linha, "!start", 5)==0))
+        if((strncmp(linha, "!start", 5)==0))//Vai inicializar o verificador para travar o funcionamento do restante do código se não tiver o Start
         {
             verificador_start+=1;
-            if(verificador_start>1)
+            if(verificador_start>1)//Tratamento de exceção para duplicação do comando start
             {
                 printf("Um comando de Start a mais foi inserido no código, remova o excedente, lembre-se que o código só irá contar apartir de quando o !start for iniciado!\n", linhas_lidas);
                 printf("Conserte o erro e tente novamente!\n");
@@ -196,30 +195,30 @@ int main()
                 exit(0);
             }
         }
-        if(verificador_start==1)
+        if(verificador_start==1 && verificador_stop==0)
         {
             if((strncmp(linha, "var", 3)==0))
             {
                 verifica_var(linha);
             }
         }
-        if(strncmp(linha, "func void main()", 16)==0 || strncmp(linha, "func void main ()", 17)==0)
+        if(strncmp(linha, "func void main()", 16)==0 && verificador_start==1 && verificador_stop==0 || strncmp(linha, "func void main ()", 17)==0 && verificador_start==1 && verificador_stop==0)//Mesmo principio do Start, mas para função Main
         {
-            if(verificador_main>1)
+            if(verificador_main>1)//Tratamento de exceção para duplicação da criação da main
             {
                 printf("Houve uma tentativa de abertura de outra main na linha %d, apague-a e deixe seu código dentro de apenas uma função main!\n", linhas_lidas);
                 printf("Conserte o erro e tente novamente!\n");
                 printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
                 exit(0);
             }
-            if(linha[strlen(linha)-2]!='{')
+            if(linha[strlen(linha)-2]!='{')//Verificar a presença da chave de abertura na main
             {
                 printf("A abertura da main faltou a chave de abertura no final da linha %d!\n", linhas_lidas);
                 printf("Conserte o erro e tente novamente!\n");
                 printf("Foram lidas %d linhas do arquivo!\n", linhas_lidas);
                 exit(0);
             }
-            else
+            else//A main irá operar sempre com um o valor 1 no verificador_chaves até reconhecer sua chave de fechamento devidamente
             {
                 verificador_main+=1;
                 verificador_chaves+=1;
@@ -230,14 +229,14 @@ int main()
             if(strncmp(linha, ))
         }*/
 
-        if(strncmp(linha, "}", 1)==0)
+        if(strncmp(linha, "}", 1)==0)//Ajustar o contador de acordo com as chaves de fechamento
         {
             verificador_chaves-=1;
         }
-        if(strncmp(linha, "!stop", 5) == 0)
+        if(strncmp(linha, "!stop", 5) == 0)//Verificador para checar se o código teve o Stop colocado devidamente
         {   
             verificador_stop+=1;
-            if(verificador_stop>1)
+            if(verificador_stop>1)//Tratamento de exceção para duplicação do comando stop
             {
                 printf("Um comando de Stop a mais foi inserido no código, remova o excedente, lembre-se que o código irá deixar de verificar depois do !stop\n", linhas_lidas);
                 printf("Conserte o erro e tente novamente!\n");
